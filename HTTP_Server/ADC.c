d8ccfd550c217bdc32f23b6ef01f6f3c9c243441
marcosgz25 <marco.santamaria.gonzalez@alumnos.upm.es>
2022-03-15 16:39:17 +0100

    TTT

--- HTTP_Server/ADC.c
+++ HTTP_Server/ADC.c
@@ -6,7 +6,7 @@
 
 /* Copyright (c) 2013 - 2014 ARM LIMITED
 
-   All rights reserved.
+   All rights reserved. TTTTT
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
    - Redistributions of source code must retain the above copyright
